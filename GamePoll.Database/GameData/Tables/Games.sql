17863ba0f5fc1562e99bb22e677a14ca3ac1bd52
Pontus Franzén <pontus.franzen@tretton37.com>
2018-01-12 12:03:06 +0100

    Add PoC Game table to project.

---
+++ GamePoll.Database/GameData/Tables/Games.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [GameData].[Games] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Title]    NVARCHAR (100) NOT NULL,
+    [ImageUrl] NVARCHAR (MAX) NULL, 
+    CONSTRAINT [PK_Games] PRIMARY KEY ([Id])
+);
+
