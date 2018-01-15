c1e462553cef8478d81cd182bebe8ba4a1958cb4
Pontus Franzén <pontus.franzen@tretton37.com>
2018-01-15 17:28:56 +0100

    Add configuration of core dependencies.

---
+++ GamePoll.Core/Infrastructure/Configuration/CoreConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+using GamePoll.Core.Infrastructure.Data;
+using GamePoll.Core.Repositories;
+using GamePoll.Core.Repositories.Entities;
+using GamePoll.Core.Services.GameData;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GamePoll.Core.Infrastructure.Configuration
+{
+    public static class CoreConfigurationExtensions
+    {
+        public static IServiceCollection AddCoreConfiguration(this IServiceCollection service, IConfiguration configuration)
+        {
+            return service
+                .AddScoped<IGameService, GameService>()
+                .AddScoped<IRepository<GameEntity>, GameRepository>()
+                .AddSingleton<IDbConnectionFactory>(factory => new SqlConnectionFactory(configuration.GetConnectionString("CoreData")));
+        }
+    }
+}
