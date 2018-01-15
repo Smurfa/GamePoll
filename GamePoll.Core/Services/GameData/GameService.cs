f431e35404dafbd29bb362dc02ce2b124409e8f5
Pontus Franzén <pontus.franzen@tretton37.com>
2018-01-15 17:27:49 +0100

    Add service for games.

---
+++ GamePoll.Core/Services/GameData/GameService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GamePoll.Core.Repositories;
+using GamePoll.Core.Repositories.Entities;
+
+namespace GamePoll.Core.Services.GameData
+{
+    public class GameService : IGameService
+    {
+        private readonly IRepository<GameEntity> _repository;
+
+        public GameService(IRepository<GameEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<GameEntity>> GetGamesAsync()
+        {
+            return await _repository.Get();
+        }
+    }
+}
---
+++ GamePoll.Core/Services/GameData/IGameService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GamePoll.Core.Repositories.Entities;
+
+namespace GamePoll.Core.Services.GameData
+{
+    public interface IGameService
+    {
+        Task<IEnumerable<GameEntity>> GetGamesAsync();
+    }
+}
